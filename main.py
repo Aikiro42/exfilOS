6f6199b0784392dd68061e4fb94339a3ab14eb49
Aikiro42 <hiroku042@gmail.com>
2026-01-08 12:46:09 +0800

    fix: various edits - Clear screen on start - Added "read" to command list

--- main.py
+++ main.py
@@ -5,78 +5,85 @@ from core.colors import color, bcolors
 import os
 import readline
 
-user:str = "user@PythonOS"
-current:File = ROOT
-current_path = current.tracePath()
+def run():
+  user:str = "user@PythonOS"
+  current:File = ROOT
+  current_path = current.tracePath()
 
-while True:
-  try:
-    cmdstr = input(f"{color(user, bcolors.PROFILE)}:{color(current_path, bcolors.CWD)}$ ")
-    cmd:Command = sh.parse(cmdstr)
-  except EOFError:
-    break
-  
-  # manual/help
-  if (cmd.exec in ("help", "man")) or ("help" in cmd.wflags) or ("?" in cmd.lflags):
-    print("There is no help.")
-  elif cmd.exec in ('exit', 'logout'):
-    break
+  os.system("cls" if os.name == "nt" else "clear")
 
-  # clear
-  if cmd.exec in ("clear", "cls"):
-    os.system("cls" if os.name == "nt" else "clear")
+  while True:
+    try:
+      cmdstr = input(f"{color(user, bcolors.PROFILE)}:{color(current_path, bcolors.CWD)}$ ")
+      cmd:Command = sh.parse(cmdstr)
+    except EOFError:
+      break
     
-  # list directories
-  elif cmd.exec in ("l", "ls"):
-    recursive = "r" in cmd.lflags or "recursive" in cmd.wflags
-    all = "a" in cmd.lflags or "all" in cmd.wflags
-    if len(cmd.args) > 0:
-      current.ls(recursive=recursive, all=all, path=cmd.args[0])
-    else:
-      current.ls(recursive=recursive, all=all)
+    # manual/help
+    if (cmd.exec in ("help", "man")) or ("help" in cmd.wflags) or ("?" in cmd.lflags):
+      print("There is no help.")
+    elif cmd.exec in ('exit', 'logout'):
+      break
 
-  # change directory
-  elif cmd.exec == "cd":
-    if len(cmd.args) > 0:
-      tgt = current.followPath(cmd.args[0], dirOnly=True)
-      if tgt is not None:
-        current = tgt
+    # clear
+    if cmd.exec in ("clear", "cls"):
+      os.system("cls" if os.name == "nt" else "clear")
+      
+    # list directories
+    elif cmd.exec in ("l", "ls"):
+      recursive = "r" in cmd.lflags or "recursive" in cmd.wflags
+      all = "a" in cmd.lflags or "all" in cmd.wflags
+      if len(cmd.args) > 0:
+        current.ls(recursive=recursive, all=all, path=cmd.args[0])
+      else:
+        current.ls(recursive=recursive, all=all)
+
+    # change directory
+    elif cmd.exec == "cd":
+      if len(cmd.args) > 0:
+        tgt = current.followPath(cmd.args[0], dirOnly=True)
+        if tgt is not None:
+          current = tgt
+          current_path = current.tracePath()
+      else:
+        current = ROOT
         current_path = current.tracePath()
-    else:
-      current = ROOT
-      current_path = current.tracePath()
-  
-  # make directory
-  elif cmd.exec == "mkdir":
-    if len(cmd.args) > 0:
-      current.mkdir(cmd.args[0])
-    else:
-      print("ERROR: Path not specified")
-  
-  # make file
-  elif cmd.exec in ("touch", "mkfile"):
-    if len(cmd.args) > 0:
-      current.touch(cmd.args[0])
-    else:
-      print("ERROR: File not specified")
+    
+    # make directory
+    elif cmd.exec == "mkdir":
+      if len(cmd.args) > 0:
+        current.mkdir(cmd.args[0])
+      else:
+        print("ERROR: Path not specified")
+    
+    # make file
+    elif cmd.exec in ("touch", "mkfile"):
+      if len(cmd.args) > 0:
+        current.touch(cmd.args[0])
+      else:
+        print("ERROR: File not specified")
 
-  # code
-  elif cmd.exec in ("code", "nano", "vim"):
-    if len(cmd.args) > 0:
-      replace = "r" in cmd.lflags or "recursive" in cmd.wflags
-      current.edit(cmd.args[0], replace)
-    else:
-      print("ERROR: File not specified")
-  
-  # view file
-  elif cmd.exec in ("cat", "view"):
-    if len(cmd.args) > 0:
-      current.cat(cmd.args[0])
-    else:
-      print("ERROR: File not specified")
-  
-  elif cmd.exec == "save":
-    exportFiles(ROOT) 
+    # code
+    elif cmd.exec in ("code", "nano", "vim"):
+      if len(cmd.args) > 0:
+        replace = "r" in cmd.lflags or "recursive" in cmd.wflags
+        current.edit(cmd.args[0], replace)
+      else:
+        print("ERROR: File not specified")
+    
+    # view file
+    elif cmd.exec in ("cat", "view", "read"):
+      if len(cmd.args) > 0:
+        current.cat(cmd.args[0])
+      else:
+        print("ERROR: File not specified")
+    
+    elif cmd.exec == "save":
+      exportFiles(ROOT) 
 
-print("")
-exportFiles(ROOT)
+  print("")
+  exportFiles(ROOT)
+
+
+if __name__ == "__main__":
+  run()
