68492f07e1b0ee2afc3b332e04e177e1295d39ed
Aikiro42 <hiroku042@gmail.com>
2026-01-09 09:03:12 +0800

    feat: add printColor function

--- core/colors.py
+++ core/colors.py
@@ -1,5 +1,8 @@
 from .const import bcolors
 from prompt_toolkit.formatted_text import ANSI
 
-def color(str:str, color:str):
-    return f"{color}{str}{bcolors.ENDC}"
+def color(str:str, ansiColor:str):
+    return f"{ansiColor}{str}{bcolors.ENDC}"
+
+def printColor(*s, ansiColor="", sep=" ", end="\n", flush=False,file=None):
+    print(*[color(str(ss), ansiColor) for ss in s], sep=sep, end=end, flush=flush, file=file)
